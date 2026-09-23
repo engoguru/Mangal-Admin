66dbcc294a579f545b116ffd36e554cbb75002cf
engoguru <engoguru@carkverma.com>
2026-09-23 13:17:11 +0530

    new url

--- src/utils/base_url.jsx
+++ src/utils/base_url.jsx
@@ -5,5 +5,5 @@ export const base_booking_url = "https://api.mangalagrahmandir.in/api/v1";
 
 
 
-export const base_booking_url2 = "http://localhost:5000/api/"; 
+export const base_booking_url2 = "https://mangal-backend-05fz.onrender.com/api/"; 
 //  export const base_booking_url2 ="https://7bcd-116-75-246-23.ngrok-free.app/api/"
